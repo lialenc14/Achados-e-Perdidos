a44201689c219a077cf92a910a66fb60b2ea9b4c
GabrielRagalzzi <ragalzzigabriel@gmail.com>
2025-11-01 23:53:54 -0300

    JS funcionando com LocalStorage

--- Js/perdidos.js
+++ Js/perdidos.js
@@ -1,11 +1,11 @@
 const nome = document.getElementById("nome");
 const descricao = document.getElementById("descricao");
-const foto = document.getElementById("data");
+const foto = document.getElementById("foto");
 const confirmar = document.getElementById("confirmar");
 const inicio = document.querySelector(".inicio");
+const limpar = document.getElementById("limpar")
 
-confirmar.addEventListener("click", function(){
-    function criarItem({ nome, imagem, descricao }) {
+function criarItem({ nome, imagem, descricao }) {
     const div = document.createElement("div");
     div.classList.add("item");
     div.innerHTML = `
@@ -13,18 +13,45 @@ confirmar.addEventListener("click", function(){
         <h3>${nome}</h3>
         <p>${descricao}</p>
         `;
-        return div;
-    }
+    return div;
+};
 
-    const item = criarItem({
-        nome: nome.value,
-        descricao: descricao.value,
-        imagem: foto.value
+function carregarItem(){
+    const itensSalvos = JSON.parse(localStorage.getItem("itens")) || []
+
+    itensSalvos.forEach(dados => {
+        const item = criarItem(dados);
+        inicio.appendChild(item);
     });
+};
 
-    inicio.appendChild(item);
-    
-    nome.value = "";
-    descricao.value = "";
-    foto.value = "";
-})
+if(confirmar){
+    confirmar.addEventListener("click", function(){
+        const NovoItem = {
+            nome: nome.value,
+            descricao: descricao.value,
+            imagem: foto.value
+        };
+        const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+        itens.push(NovoItem);
+
+        localStorage.setItem("itens", JSON.stringify(itens));
+
+        const item = criarItem(NovoItem);
+        inicio.appendChild(item);
+
+        nome.value = "";
+        descricao.value = "";
+        foto.value = "";
+    })
+};
+
+if(limpar){
+    limpar.addEventListener("click", function(){
+        localStorage.removeItem("itens");
+        inicio.innerHTML = `<button id="limpar">Limpar lista</button>`;
+    })
+};
+
+window.addEventListener("DOMContentLoaded", carregarItem);
